26962e8836022d4823974de1930a41b810bc16ae
Adam Jakubczak <adam.per.jakubczak@gmail.com>
2024-08-20 13:03:31 +0200

    określenie pozycji punktów rozpoznanych bloków oraz dodatkowe cechy tekstowe dotyczące słów

--- raport/extract/feats.jl
+++ raport/extract/feats.jl
@@ -1,6 +1,6 @@
-module Features
-  export docnum, catnum, clmnum, txtnum, intol, extract
-  using DataFrames
+  module Features
+export docnum, catnum, clmnum, txtnum, intol, extract
+using DataFrames
 
     "typ etykiety tekstu - referencja dokumentu"      const docnum = 1
     "typ etykiety tekstu - kategoria dokument"        const catnum = 2
@@ -14,7 +14,6 @@ elseif type == "category" return catnum
 elseif type == "claims"   return clmnum
 else                      return txtnum end
 end
-
 
     using StringDistances
   function intol(test::String, queries::Vector{String}, tol::Float64)
@@ -58,40 +57,95 @@ const btmkeys = ["strona x z x",
 fdf = DataFrame(unit=Int[], 
                 group=Int[], 
                 type=Int[],
-                pt0=Pt[], 
-                pt1=Pt[], 
-                pt2=Pt[], 
-                pt3=Pt[],
-                length=Int[], 
-                lowercase=Int[], 
-                uppercase=Int[], 
-                digit=Int[],
-                #keywords
+                ptoplft=Pt[], 
+                ptoprgt=Pt[], 
+                pbtmlft=Pt[], 
+                pbtmrgt=Pt[],
+                length=Int[],
                 thdockeys=Int[],  #table-header-docs
                 thcatkeys=Int[],  #table-header-category
                 thclmkeys=Int[],  #table-header-claims
-                btmkeys=Int[])    #bottom-line
+                btmkeys=Int[],    #bottom-line
+                abbrs=Int[],      #skróty
+                strtabbr=Bool[],  #początek
+                solwords=Int[],   #pojedyncze
+                sentences=Int[],
+                shtcodes=Int[],   #krótkie
+                lngcodes=Int[])   #długie
 fmap = Dict{Tuple{String, Int}, Int}()
-for row in eachrow(df) 
+  for row in eachrow(df) 
+unit = (row.file, row.page)
+fmap[unit] = get(fmap, unit, length(fmap) + 1)
 
-  unit = (row.file, row.page)
-  fmap[unit] = get(fmap, unit, length(fmap) + 1)
-  
+pts = [(row.px0, row.py0),
+        (row.px1, row.py1),
+        (row.px2, row.py2),
+        (row.px3, row.py3)]
+sort!(pts, by=pt->pt[2])
+top, btm = pts[1:2], pts[3:4]
+sort!(pts, by=pt->pt[1])
+lft, rgt = pts[1:2], pts[3:4]
+
+sentences = Text.wordchain(row.text) |> Text.sententify
   push!(fdf, (fmap[unit],
-    row.group, encode(row.type),
-    (row.px0, row.py0), 
-    (row.px1, row.py1),
-    (row.px2, row.py2), 
-    (row.px3, row.py3),
-    length(row.text), 
-    count(islowercase, row.text),
-    count(isuppercase, row.text),
-    count(isdigit, row.text),
-    intol(row.text, thdockeys, 0.2),
-    intol(row.text, thcatkeys, 0.2),
-    intol(row.text, thclmkeys, 0.1),
-    intol(row.text, btmkeys, 0.1)))
-  end
+row.group, encode(row.type),
+commonpt(top, lft),
+commonpt(top, rgt),
+commonpt(btm, lft),
+commonpt(btm, rgt),
+length(row.text),
+intol(row.text, thdockeys, 0.2),
+intol(row.text, thcatkeys, 0.2),
+intol(row.text, thclmkeys, 0.1),
+intol(row.text, btmkeys, 0.1),
+any(uppercase(x) == x && length(x) < 4 for x in sentences),
+uppercase(sentences[1]) == sentences[1] && length(sentences[1]) < 4,
+count(x -> all(isletter, x) && !(' ' in x), sentences),
+count(x -> ' ' in x, sentences),
+count(x -> all(isdigit, x) && length(x) < 4, sentences),
+count(x -> all(isdigit, x) && length(x) >= 4, sentences)
+))end#for
 return fdf
+end#extract
+  function commonpt(A, B)::Pt
+for a in A
+  for b in B
+    if a[1] == b[1] && a[2] == b[2] return a end
+    end
+  end
+  return A[1]
+  end
+
+    module Text
+export wordchain, sententify
+  """Zwraca listę słów z tekstu oraz znaków które je oddzielają"""
+function wordchain(string::String)
+chained = split(string, r"((?=[^a-zA-Z0-9\p{L}])|(?<=[^a-zA-Z0-9\p{L}]))")
+chained = filter(x -> !isempty(x), chained)
+return chained
 end
+  """Łączy słowa złożone z liter w sentencje"""
+function sententify(strings::Union{Vector{String}, Vector{SubString{String}}})
+sentified::Vector{String} = [strings[1]]
+lttrprv = all(isletter, strings[1])
+prvsent = lttrprv
+for string in strings[2:end]
+  if all(isletter, collect(string))
+    lttrprv = true
+    if prvsent
+      sentified[end] = sentified[end] * " " * string
+    else
+      push!(sentified, string)
+      prvsent = true
+      end
+  elseif lttrprv && (all(isspace, collect(string)) || string == ".")
+    sentified[end] = sentified[end] * string
+  elseif all(x -> isletter(x) || isdigit(x), collect(string)) 
+    push!(sentified, string) 
+    prvsent = false
+  else prvsent = false end
+end#for
+return sentified
+  end#function
+end#TextFeatures
   end#module
---
+++ raport/labels/meta.jl
@@ -0,0 +1,25 @@
+  module DatasetMeta
+export type
+type = Dict(
+  "file" => String,
+  "text" => String, 
+  "page" => Int,
+  "x0" => Int, 
+  "x1" => Int, 
+  "x2" => Int, 
+  "x3" => Int,
+  "y0" => Int, 
+  "y1" => Int, 
+  "y2" => Int, 
+  "y3" => Int,
+  "px0" => Float64, 
+  "px1" => Float64, 
+  "px2" => Float64, 
+  "px3" => Float64,
+  "py0" => Float64, 
+  "py1" => Float64, 
+  "py2" => Float64, 
+  "py3" => Float64,
+  "type" => String, 
+  "group" => Int
+)end
