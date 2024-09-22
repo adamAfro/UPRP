2c3d6f0bb684ce8eb784d17478227eb40f0b89da
adamAfro <adam.per.jakubczak@gmail.com>
2024-09-22 13:44:33 +0200

    poprawka wyszukiwania tekstów

--- raport/extract.mod.py
+++ raport/extract.mod.py
@@ -1,5 +1,4 @@
-import os, pandas, sklearn.preprocessing as prep, joblib
-os.chdir("/storage/home/ajakubczak/Dokumenty/UPRP/raport")
+import pandas
 
 def matchkey(text:str, key:str, tol:float):
   key = key.lower()
@@ -10,45 +9,56 @@ def matchkey(text:str, key:str, tol:float):
       return True
   return False
 
-def Gfeat(G:pandas.DataFrame, keys:list[str]):
+def Gfeat(G:pandas.DataFrame, searches:list[list[str]]):
   from pandas import DataFrame
   from numpy import sqrt, sign, inf
   X = DataFrame({'x': G[['xtoplft', 'xtoprgt', 'xbtmrgt', 'xbtmlft']].mean(axis=1),
                  'y': G[['ytoplft', 'ytoprgt', 'ybtmrgt', 'ybtmlft']].mean(axis=1)})
-  X['width'] = (G['xtoprgt'] - G['xtoplft'] + 
-                G['xbtmrgt'] - G['xbtmlft']) / 2
-  X['height'] = (G['ytoplft'] - G['ybtmlft'] +
-                 G['ytoprgt'] - G['ybtmrgt']) / 2
+  X['width'] = (G['xtoprgt'] - G['xtoplft'] + G['xbtmrgt'] - G['xbtmlft']) / 2
   X['length'] = G['text'].apply(len)
-  X['letters'] = G['text'].apply(lambda x: sum(c.isalpha() for c in x))
   X['digits'] = G['text'].apply(lambda x: sum(c.isdigit() for c in x))
-  X['spaces'] = G['text'].apply(lambda x: sum(c.isspace() for c in x))
-  X['puncts'] = G['text'].apply(lambda x: sum(c in '.,;:?!' for c in x))
   X['words'] = G['text'].apply(lambda x: len(x.split()))
-  for key in keys:
-    X[f"#{key}"] = G['text'].apply(lambda x: matchkey(x, key, 0.5))
-    X[f"x&{key}"] = X[f"#{key}"].astype(float)#closeness
-    X[f"y&{key}"] = X[f"#{key}"].astype(float)#closeness
+  keys = set([key for search in searches for key in search])
+  for k in keys:
+    X[f"#{k}"] = G['text'].apply(lambda x: matchkey(x, k, 0.5))
+  for s in searches:
+    X[f"#{s}"] = X[[f"#{k}" for k in s]].all(axis=1)
+    for i, x in X[~X[f"#{s}"] & X[f"#{s[0]}"]].iterrows():
+      column = (X['x'] - x['x']).abs() < x['width']/2
+      below = X['y'] - x['y'] > 0
+      N = X.loc[column & below].copy()
+      N['diff'] = (N['x'] - x['x'])**2 + (N['y'] - x['y'])**2
+      N = N.sort_values('diff').head(3)
+      if N.empty: continue
+      if N[f"#{s[1]}"].any(): X.loc[i, f"#{s}"] = True
+    X[f"x&{s}"] = X[f"#{s}"].astype(float)#closeness
+    X[f"y&{s}"] = X[f"#{s}"].astype(float)#closeness
+    X[f"X&{s}"], X[f"Y&{s}"] = 0.0, 0.0
+  X = X.drop(columns=[f"#{k}" for k in keys])
   for i, row in X.iterrows():
-    for key in keys:
+    for s in searches:
+      if row[f"#{s}"]: continue
       mindist = inf
-      for j, krow in X[ X[f"#{key}"] == True ].iterrows():
+      for j, krow in X[ X[f"#{s}"] == True ].iterrows():
         if i == j: continue
         xd, yd = row['x'] - krow['x'], row['y'] - krow['y']
         dist = sqrt(xd**2 + yd**2)
         if dist >= mindist: continue
         else: mindist = dist
-        X.loc[i, f"x&{key}"] = sign(xd)/(1 + abs(xd))
-        X.loc[i, f"y&{key}"] = sign(yd)/(1 + abs(yd))
-  return X.drop(columns=[f"#{key}" for key in keys])
+        X.loc[i, f"x&{s}"] = 1/(1 + abs(xd))
+        X.loc[i, f"y&{s}"] = 1/(1 + abs(yd))
+        X.loc[i, f"X&{s}"] = -1.0 if xd < 0 else 1.0
+        X.loc[i, f"Y&{s}"] = -1.0 if yd < 0 else 1.0
+  X = X.drop(columns=[f"#{s}" for s in searches])
+  return X
 
-def feat(df:pandas.DataFrame, keys:list[str]):
+def feat(df:pandas.DataFrame, searches:list[str]):
   from concurrent.futures import ProcessPoolExecutor, as_completed
   from pandas import concat
   from tqdm import tqdm
   result=[]
   with ProcessPoolExecutor() as exc:
-    fus = [exc.submit(Gfeat, G, keys) for _, G in df.groupby('unit')]
+    fus = [exc.submit(Gfeat, G, searches) for _, G in df.groupby('unit')]
     for fu in tqdm(as_completed(fus), desc="📏", total=len(fus)):
       result.append(fu.result())
   return concat(result, ignore_index=True)
@@ -68,24 +78,29 @@ def neural(nneuro:list[int], drop=0.0):
     M.add(Dense(d, activation='relu'))
     if drop > 0.0: M.add(Dropout(drop))
   M.add(Dense(1, activation='sigmoid'))
-  M.compile(loss='binary_crossentropy', optimizer=Adam(0.0005), metrics=['accuracy'])
+  M.compile(loss='binary_crossentropy', optimizer=Adam(0.001), metrics=['accuracy'])
   return M
 
-keys = ["kategoria", "dokument", "identyfikacja", "odniesienie", "zastrzeżenie",
-        "podważający", "poziom", "sprawozdanie", "wykonał"]
+searches = [["kategoria", "dokumentu"],
+            ["dokumenty", "z", "podana", "identyfikacja"],
+            ["odniesienie", "do", "zastrzeżenie"],
+            ["podważający", "poziom"],
+            ["sprawozdanie", "wykonał"]]
 
 df = pandas.read_csv('labels/labeled.csv')
 tr, ts = split(df, unit='unit')
-Xtr, ytr = feat(tr, keys), tr['type'] != 0
-Xts, yts = feat(ts, keys), ts['type'] != 0
+Xtr, ytr = feat(tr, searches), tr['type'] != 0
+Xts, yts = feat(ts, searches), ts['type'] != 0
 
+from sklearn import preprocessing as prep
 scl = prep.StandardScaler()
 Xtr = scl.fit_transform(Xtr)
 Xts = scl.transform(Xts)
+import joblib
 joblib.dump(scl, 'scaler.joblib')
 
-model = neural([Xtr.shape[1], 512, 512, 256], drop=0.0)
+model = neural([Xtr.shape[1], 512, 512, 256], drop=0.5)
 his = model.fit(Xtr, ytr, validation_data=(Xts, yts),
-                epochs=100, batch_size=2000, verbose=2).history
+                epochs=1000, batch_size=2000, verbose=2).history
 
 model.save('extractor.keras')
