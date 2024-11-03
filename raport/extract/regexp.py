e9ed8a32dcf08b64efb373262f3734dd29975c95
adamAfro <adam.per.jakubczak@gmail.com>
2024-11-03 16:30:09 +0100

    wyrażenia regularne i przycinanie outputu

--- raport/extract/regexp.py
+++ raport/extract/regexp.py
@@ -31,8 +31,9 @@ def patregex( code = r'\b[a-z]{1,3}\.?(\s[a-z]{1,2}\.?)?', cforce = True,
     alpha = rf"({alpha})?{code}({alpha})?"
   return rf"{pre}{alpha}\s*{digits}{suf}"
 
-def dateregex(y=4, spaced=True, daymonth=True):
+def dateregex(y=4, spaced=True, daymonth=True, ygreedy=False):
   y = r"(20[012]\d|19\d{2})" if y == 4 else r"\d{2}"
+  if ygreedy: y = rf"{y}(\s*r(?!\w))\.?"
   if spaced: y = rf"{y}(\s*r(?!\w))?\.?"
   D = y
   if daymonth:
@@ -49,12 +50,11 @@ with open(os.path.join('month.json'), 'r') as f: month = json.load(f)
 
 Q = dict(
   
-  etal = r'\b(et\s*al\.?|i\s*in{1,2}i?\.?)\b',
-  pub = r"(wyd?a?n?i?e?|vol|publ?i?k?a?c?j?a?)\.?\s*(\d\s*)+",
-  pgnum = r'\b((?<!(?<=p)l|\s)\s*p{1,2}|s(tr)?)\.?\s*\d{1,5}\s+\d{1,5}\b',
+  etal = r'\b(et\s*al|i\s*in{1,2}i?)\s*\.?\b',
+  pub = r"(wyd(anie)?|vol(ume)?|publ?(ikacja)?|iss(ue)?)\.?\s*(\d\s*)+",
+  pgnum = r'\b((?<!(?<=p)l|\s)\s*p{1,2}|\s*s(tr)?)\.?\s*\d{1,5}\s+\d{1,5}\b',
 
-  LDOI = r"(https\s*)?\s*doi\.?\s*[\d\s\.]+",
-  RDOI = r"[\d\s\.]+\s*(ht{2}ps?\s*)?doi\.?\b",
+  DOI = r"(https\s*)?\s*doi\.?\s*[\d\s\.]+",
 
   code = patregex(cforce=False),
   EP = patregex(r'ep\.?\s*[^\Wp]?\s*\w?'),
@@ -68,13 +68,16 @@ Q = dict(
   Rmonth = rf"\s*\d*({'|'.join([ m for M in month['PL'] + month['EN'] for m in M ])})\s*\d+",
   fullmonth = rf"({'|'.join([ M[0] for M in month['PL'] + month['EN'] ] + [M[1] for M in month['PL']])})",
   yearnum = dateregex(4, daymonth=False),
-  datealt = dateregex(2),
+  datealt = dateregex(2, ygreedy=True),
+  datenumy = dateregex(4, ygreedy=True),
   datenum = dateregex(4),
   datealt0 = dateregex(2, spaced=False),
   datenum0 = dateregex(4, spaced=False),
   
   supgroup = '|'.join([rf'\{a}.*[\{{\}}\[\]\(\)\"].*\{b}' for a,b in ['()', '[]', '{}', '""']]),
   group = '|'.join([rf'\{a}.{{3,}}\{b}' for a,b in ['()', '[]', '{}', '""']]),
+  sep = r'[,;:\(\)\[\]\{\}\'"]',
+  alnum = r'\b([a-z]+\d+\w*|\d+[a-z]\w*)\b',
   URL = r'http[s]?://(?:\w|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',
 )
 
@@ -94,14 +97,17 @@ F = [
     for i, (t, a, m, N) in enumerate(extract(t0, Q['group'], N0))],
 
   txtFgen('URL'),
-  Fgen('datenum'),
-  Fgen('LDOI'), Fgen('RDOI'),
+  Fgen('pub'),
+  Fgen('datenumy'), Fgen('datenum'),
+  Fgen('DOI'),
   Fgen('EP'), Fgen('PL'), Fgen('code'),
   Fgen('Lmonth'), Fgen('Rmonth'),
   Fgen('EP56'), Fgen('PL56'), Fgen('code56'),
 
-  Fgen('pgnum'), Fgen('pub'), Fgen('etal'),
+  Fgen('pgnum'), Fgen('etal'),
   Fgen('datealt'), Fgen('yearnum'), Fgen('fullmonth'),
+  Fgen('alnum'),
+  txtFgen('sep'),
 ]
 
 def chain(t:str, N:str, a:int=0):
@@ -125,13 +131,13 @@ X = X.progress_apply(lambda x: [{ "docs": x['docs'], "text": t, "norm": N, "star
                                 for t, N, a, M in chain(x['text'], x['norm'])], axis=1)
 X = DataFrame(X.explode().tolist())
 X.insert(4, "end", X['start'] + X['text'].str.len() - 1)
-X = X[X['start'] <= X['end']]
+X = X[(X['start'] <= X['end'])&(~X['sep'])&(X['text'].str.strip() != '')]
 
 with warnings.catch_warnings():
   warnings.simplefilter("ignore", UserWarning)
 
-  for k0 in ['EP', 'PL', 'code']:
-    for k in ['EP56', 'PL56', 'code56', 'pgnum', 'pub', 'yearnum', 'datealt0', 'datenum0']:
+  for k0 in ['EP', 'PL', 'code', 'pub']:
+    for k in ['EP56', 'PL56', 'code56', 'pgnum', 'yearnum', 'datealt0', 'datenum0', "DOI"]:
       X.loc[X[k0], k] = X.loc[X[k0], 'norm'].str.contains(Q[k], regex=True)
   for k0 in ['Lmonth', 'Rmonth']:
     X.loc[X[k0], "fullmonth"] = X.loc[X[k0], 'norm'].str.contains(Q["fullmonth"], regex=True)
@@ -146,6 +152,16 @@ for i, k0 in enumerate(M0.index):
     r = round(y / X.shape[0] * 100, 2)
     M0.loc[k0, k] = f"{r:.2f}" if r > 0.0 else '0+  ' if y > 0 else '0   '
 print("frac. X[x]&X[y]", M0, sep='\n')
+
+X['lstrip'] = X['text'].str.replace(r'^[^\w\(\)\[\]"\.]+', "", regex=True)
+X['start'] = X['start'] + X['text'].str.len() - X['lstrip'].str.len()
+X['text'] = X['lstrip']
+X = X.drop(columns=['lstrip'])
+
+X['rstrip'] = X['text'].str.replace(r'[^\w\(\)\[\]"\.]+$', "", regex=True)
+X['end'] = X['end'] - X['text'].str.len() + X['rstrip'].str.len()
+X['text'] = X['rstrip']
+X = X.drop(columns=['rstrip'])
 
 X.drop(columns=["norm"]).to_csv('docs.chunks.csv', index=False)
 
