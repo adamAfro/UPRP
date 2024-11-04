638a54cc6d33f4662c3a4514f9ceca307bd022ce
Adam Jakubczak <adam.per.jakubczak@gmail.com>
2024-11-04 23:23:23 +0100

    uproszczenie wyszukiwania wyrażeń

--- raport/extract/code.py
+++ raport/extract/code.py
@@ -1,126 +1,46 @@
 from pandas import read_csv, DataFrame
 from tqdm import tqdm as progress; progress.pandas()
-import os, json
+import os, json, re
 
-def extract(x:str, q:str):
-  "-> list[(sliced x, start index, q-match-bool]"
+with open(os.path.join('month.json'), 'r') as f: Mo = json.load(f)
+mo = '|'.join([ m for M in Mo['PL'] + Mo['EN'] for m in M ])
+
+Q = re.compile('(?:' + '|'.join(rf'(?P<{k}>{q})' for k, q in {
+"month":  rf"(?:(?<=\d\s|.\d)(?:{mo})(?!\w)|(?<!\w)(?:{mo})(?=\s*\d))",
+"alnum":  r"(?<!\w)(?:[^\W\d]+\d|\d+[^\W\d])\w*(?!\w)",
+"series": "(?:" + '|'.join([rf"(?:\d+\s*{s}+\s*)+" for s in ['\.', '\-', '/', '\\', '\s']]) + ")\s*\d+",
+"num":    r"(?<!\w)\d+(?!\w)",
+"braced": '|'.join([rf"\{a}\w{1,4}\{b}" for a,b in ['()','{}','[]', '""', '<>']]),
+"abbr":   r"(?<!\w)[^\W\d]{1,4}\.?(?!\w)",
+"space":  r"[\s\-\/\\\.]+",
+
+}.items()) + ')')
+
+K0 = ['alnum', 'num', 'series']
+def expr(x:str):
   import re
-  M0 = list(re.finditer(q, x, flags=re.IGNORECASE))
-  I = [i for m in M0 for i in m.span()]
-  M = [i for i in I if any((i == m.start()) for m in M0)]
-  if len(I) == 0: return [(x, 0, False)]
-  if I[0] != 0: I = [0] + I
-  I = I + [len(x)]
-  Y = [x[I[i]:I[i+1]] for i in range(len(I)-1)]
-  Y = [(Y[i], I[i], I[i] in M) for i in range(len(Y))]
-  return Y
+  global Q, K0
+  I = re.finditer(Q, x)
+  S = [(m.group(), m.start(), m.end(), m.groupdict()) for m in I]
+  if not S: return []
+  S = [(x, a, b, any(m[k] is not None for k in K0)) for x, a, b, m in S]
+  S.sort(key=lambda x: x[1])
+  M = [(S[0])]
+  for x, a, b, m in S[1:]:
+    x0, a0, b0, m0 = M[-1]
+    if a > b0: M.append((x, a, b, m))
+    else:
+      M[-1] = (x0 + x, a0, max(b0, b), m0 or m)
+  return M
 
-L0, R0 = r"(?<=\d\s|.\d)", r"(?=\s*\d)"
-Q = dict()
-
-Q["URL"] = r'http[s]?://(?:\w|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
-
-brace = '|'.join([rf"\{a}\w+\{b}" for a,b in ['()','{}','[]', '""', '<>']])
-brace = rf"(?:{brace})"
-Q["braceX"] = rf"{L0}{brace}{R0}"
-Q["braceL"] = rf"(?<!\w){brace}{R0}"
-Q["braceR"] = rf"{L0}{brace}(?!\w)"
-
-with open(os.path.join('month.json'), 'r') as f: month = json.load(f)
-month = '|'.join([ m for M in month['PL'] + month['EN'] for m in M ])
-Q["monthX"] = rf"{L0}(?:{month}){R0}"
-Q["monthR"] = rf"{L0}(?:{month})(?!\w)"
-Q["monthL"] = rf"(?<!\w)(?:{month}){R0}"
-
-sep = [r'\-', r'/', r'\\']
-Q['alseriesL'] = rf"(?<!\w)\w{{,3}}[^\W\d]\.?\s*[{''.join(sep)}]{R0}"
-Q['alseriesR'] = rf"{L0}[{''.join(sep)}]\s*[^\W\d]\w{{,3}}\.?(?!\w)"
-Q['braceseriesL'] = rf"(?<!\w){brace}\s*[{''.join(sep)}]{R0}"
-Q['braceseriesR'] = rf"{L0}[{''.join(sep)}]\s*{brace}(?!\w)"
-
-abbr = r"[^\W\d]{1,4}\.?"
-Q["abbrX"] = rf"{L0}{abbr}(?:\s+{abbr})*{R0}"
-Q["abbrL"] = rf"(?<!\w){abbr}(?:\s+{abbr})*{R0}"
-Q["abbrR"] = rf"{L0}{abbr}(?:\s+{abbr})*(?!\w)"
-
-sep += [r'\.'] + sep + [r'\s']
-Q['series0'] = "(?:" + '|'.join([rf"(?:\d+\s*{s}+\s*)+" for s in sep]) + ")\s*\d+"
-Q['seriesU'] = rf"(?:\d+\s*[{''.join(sep)}]+\s*)+\s*\d+"
-
-alnum = r"(?:[^\W\d]+\d|\d+[^\W\d])\w*"
-Q["alnum"] = rf"(?<!\w){alnum}(?!\w)"
-
-Q['num'] = r"(?<!\w)\d+(?!\w)"
-
-K = Q.keys()
-def extractk(x):
-  global K
-  X = [{ "type": None, "start": 0, **x }]
-  for k in K:
-    Y = [(x, extract(x['text'], Q[k])) if x['type'] is None
-         else (x, [(x['text'], 0, False)]) for x in X]
-    X = [{ **x, "text": t, "start": i + x['start'], "type": k if m else x['type'] } 
-         for x, y in Y for t, i, m in y]
-  return X
-
-m0 = ['num', 'series', 'series0', 'alnum', 'altseries']
-mR = [k for k in K if k.endswith('L') or k.endswith('X') if k != "URL"]
-mL = [k for k in K if k.endswith('R') or k.endswith('X') if k != "URL"]
-d0 = 3
-def kmerge(X):
-  global K, m0, mL, mR, d0
-  for x0 in X:
-    x0['typeL'] = x0['typeR'] = x0['type']
-    x0['end'] = x0['start'] + len(x0['text']) - 1
-  X = [x for x in X if x['text'].strip() != ""]
-  i, n = 0, len(X)
-  while (i < n):
-    #torghtmerge
-    x0 = X[i]
-    while (x0['typeR'] is not None) and (i+1 <  n) and (X[i+1]['typeL'] is not None):
-      x = X[i+1]
-      d = x['start'] - x0['end']
-      if not ((d < d0) and (x0['typeR'] in mR) and (x['typeL'] in m0)): break
-      x0['text'] = x0['text'] + (d-1)*' ' + x['text']
-      x0['end'] = x['end']
-      x0['typeR'] = x['typeR']
-      X.pop(i+1)
-      n -= 1
-    #toleftmerge
-    while (x0['typeL'] is not None) and (i-1 >= 0) and (X[i-1]['typeR'] is not None):
-      x = X[i-1]
-      d = x0['start'] - x['end']
-      if not ((d < d0) and (x0['typeL'] in mL) and (x['typeR'] in m0)): break
-      x0['text'] = x['text'] + (d-1)*' ' + x0['text']
-      x0['start'] = x['start']
-      x0['typeL'] = x['typeL']
-      X.pop(i-1)
-      n -= 1
-      i -= 1
-    i += 1
-
-  i, n = 0, len(X)
-  while (i < n):
-    x0 = X[i]
-    while (x0['typeR'] is not None) and (i+1 <  n) and (X[i+1]['typeL'] is not None):
-      x = X[i+1]
-      d = x['start'] - x0['end']
-      if not ((d < d0) and (x0['typeR'] in m0) and (x['typeL'] in m0)): break
-      x0['text'] = x0['text'] + d*' ' + x['text']
-      x0['end'] = x['end']
-      x0['typeR'] = x['typeR']
-      X.pop(i+1)
-      n -= 1
-    i += 1
-  return X
-
-X = read_csv('../docs.csv').reset_index()#.sample(24000, random_state=0)
+X = read_csv('../docs.csv').reset_index().sample(10000)
 X = X.rename(columns={'docs':'text', 'index':'docs'})[['docs', 'text']]
-f = lambda x: kmerge(extractk(x.to_dict()))
-X = DataFrame(X.progress_apply(f, axis=1).explode().tolist())
-X = X.drop(columns=['typeL', 'typeR'])
-X = X.drop(X.query("text.str.match(r'^[\W_]+$')").index)
-X['code'] = ((~X['type'].isna())&(X['type'] != 'URL')).astype(int)
-X['end'] = X['start'] + X['text'].str.len() - 1
-X = X[['docs', 'start', 'end', 'text', 'code']]
-X.convert_dtypes().to_csv('code.csv', index=False)
+
+qURL=r'(http[s]?://(?:\w|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)'
+X['text'] = X['text'].progress_apply(lambda x:
+  re.sub(qURL, lambda m: ' '*len(m.group()), x))
+
+f = lambda x: [(x['docs'], *c) for c in expr(x['text'])]
+U = DataFrame(X.progress_apply(f, axis=1).explode().tolist(),
+               columns=['docs', 'text', 'start', 'end', 'numerical'])\
+               .dropna().query('numerical==True')
