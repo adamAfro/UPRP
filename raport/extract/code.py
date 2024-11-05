eaacee7f66274098bad53e405c0f70e021e0809d
Adam Jakubczak <adam.per.jakubczak@gmail.com>
2024-11-05 23:40:32 +0100

    klasa zamiast funkcji wyrażeń

--- raport/extract/code.py
+++ raport/extract/code.py
@@ -2,48 +2,24 @@ from pandas import read_csv, DataFrame
 from tqdm import tqdm as progress; progress.pandas()
 import os, json, re
 
-with open(os.path.join('month.json'), 'r') as f: Mo = json.load(f)
-mo = '|'.join([ m for M in Mo['PL'] + Mo['EN'] for m in M ])
-
-Q = re.compile('(?:' + '|'.join(rf'(?P<{k}>{q})' for k, q in {
-"month":  rf"(?:(?<=\d\s|.\d)(?:{mo})(?!\w)|(?<!\w)(?:{mo})(?=\s*\d))",
-"alnum":  r"(?<!\w)(?:[^\W\d]+\d|\d+[^\W\d])\w*(?!\w)",
-"series": "(?:" + '|'.join([rf"(?:\d+\s*{s}+\s*)+" for s in ['\.', '\-', '/', '\\', '\s']]) + ")\s*\d+",
-"num":    r"(?<!\w)\d+(?!\w)",
-"braced": '|'.join([rf"\{a}\w{1,4}\{b}" for a,b in ['()','{}','[]', '""', '<>']]),
-"abbr":   r"(?<!\w)[^\W\d]{1,4}\.?(?!\w)",
-"space":  r"[\s\-\/\\\.]+",
-
-}.items()) + ')')
-
-K0 = ['alnum', 'num', 'series']
-def expr(x:str):
-  import re
-  global Q, K0
-  I = re.finditer(Q, x)
-  S = [(m.group(), m.start(), m.end(), m.groupdict()) for m in I]
-  if not S: return []
-  S = [(x, a, b, any(m[k] is not None for k in K0)) for x, a, b, m in S]
-  S.sort(key=lambda x: x[1])
-  M = [(S[0])]
-  for x, a, b, m in S[1:]:
-    x0, a0, b0, m0 = M[-1]
-    if a > b0: M.append((x, a, b, m))
-    else:
-      M[-1] = (x0 + x, a0, max(b0, b), m0 or m)
-  return M
-
-X = read_csv('../docs.csv').reset_index()
-X = X.rename(columns={'docs':'text', 'index':'docs'})[['docs', 'text']]
-
-qURL=r'(http[s]?://(?:\w|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)'
-X['text'] = X['text'].progress_apply(lambda x:
-  re.sub(qURL, lambda m: ' '*len(m.group()), x))
-
-f = lambda x: [(x['docs'], *c) for c in expr(x['text'])]
-U = DataFrame(X.progress_apply(f, axis=1).explode().tolist(),
-               columns=['docs', 'text', 'start', 'end', 'numerical'])\
-               .dropna().query('numerical==True')
+class Expr:
+  "przechowuje regexy `Q` z nazwanymi grupami i nazwy `K0` tych grup co są `True`"
+  def __init__(self, Q:dict, K0:list=[]): self.Q, self.K0 = Q, K0
+  def union(self, x:str):
+    "->(złączone znajdy `Q`, początek, koniec, czy nazwa znajdy jest w `K0`)"
+    import re
+    I = re.finditer(self.Q, x)
+    S = [(m.group(), m.start(), m.end(), m.groupdict()) for m in I]
+    if not S: return []
+    S = [(x, a, b, any(m[k] is not None for k in self.K0)) for x, a, b, m in S]
+    S.sort(key=lambda x: x[1])
+    M = [(S[0])]
+    for x, a, b, m in S[1:]:
+      x0, a0, b0, m0 = M[-1]
+      if a > b0: M.append((x, a, b, m))
+      else:
+        M[-1] = (x0 + x, a0, max(b0, b), m0 or m)
+    return M
 
 def date(d:str|None, m:str|None, y:str):
   from datetime import date as f
@@ -67,6 +43,9 @@ def date(d:str|None, m:str|None, y:str):
     if D.year > 2024: return None
     return (None if d0 else D.day, None if m0 else D.month, D.year)
   except ValueError: return None
+  
+with open(os.path.join('month.json'), 'r') as fN: Mo = json.load(fN)
+mo = '|'.join([ m for M in Mo['PL'] + Mo['EN'] for m in M ])
 
 def datenum(x:str, Q = [q for s, qd, qY4, qNd in [(
   r"[\W\s]{1,3}", r"(?:3[01]|[012]?\d)", r"(?:20[012]\d|19\d\d)", r"(?:3[2-9]|[4-9]\d)")] 
@@ -89,6 +68,27 @@ def month(x:str, Q = [(i,q) for s0, s, qYd, M in [(r"[\W\s]{0,3}", r"[\W\s]{1,3}
   V = [(r,x,date(d.get('A', None), m, d.get('B'))) for r,x,d, m in Mm] +\
       [(r,x,date(d.get('B', None), m, d.get('A'))) for r,x,d, m in Mm]
   return [(a,b,x,*D) for (a,b),x,D in V if D]
+
+X = read_csv('../docs.csv').reset_index()
+X = X.rename(columns={'docs':'text', 'index':'docs'})[['docs', 'text']]
+
+qURL=r'(http[s]?://(?:\w|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)'
+X['text'] = X['text'].progress_apply(lambda x:
+  re.sub(qURL, lambda m: ' '*len(m.group()), x))
+
+eN = Expr(Q=re.compile('(?:' + '|'.join(rf'(?P<{k}>{q})' for k, q in {
+  "month":  rf"(?:(?<=\d\s|.\d)(?:{mo})(?!\w)|(?<!\w)(?:{mo})(?=\s*\d))",
+  "alnum":  r"(?<!\w)(?:[^\W\d]+\d|\d+[^\W\d])\w*(?!\w)",
+  "series": "(?:" + '|'.join([rf"(?:\d+\s*{s}+\s*)+" for s in ['\.', '\-', '/', '\\', '\s']]) + ")\s*\d+",
+  "num":    r"(?<!\w)\d+(?!\w)",
+  "braced": '|'.join([rf"\{a}\w{{1,4}}\{b}" for a,b in ['()','{}','[]', '""', '<>']]),
+  "abbr":   r"(?<!\w)[^\W\d]{1,4}\.?(?!\w)",
+  "space":  r"[\s\-\/\\\.]+",
+}.items()) + ')'), K0=['alnum', 'num', 'series'])
+fN = lambda x: [(x['docs'], *c) for c in eN.union(x['text'])]
+U = DataFrame(X.progress_apply(fN, axis=1).explode().tolist(),
+               columns=['docs', 'text', 'start', 'end', 'numerical'])\
+               .dropna().query('numerical==True')
 
 fD = lambda x: [(x['docs'], x['start'], x['end'], *c) for c in datenum(x['text'])]
 D = DataFrame([y for u in U.progress_apply(fD, axis=1) if u for y in u],
