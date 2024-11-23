3007179fe1d86d7e24aa31d9dc422a9c20332154
adamAfro <adam.per.jakubczak@gmail.com>
2024-11-23 18:50:09 +0100

    przenieś. profil. node/profile -> lib/profile

---
+++ lib/profile.py
@@ -0,0 +1,91 @@
+import os, json, xmltodict
+from pandas import DataFrame
+from tqdm import tqdm as progress
+from uuid import uuid1 as unique
+
+def xmlbundleload(f0):
+  with open(f0) as f: F = f.read()
+  F = F.replace("&", "?").split('<?xml version="1.0" encoding="UTF-8"?>')
+  F = [d for d0 in F for d in d0.split('<?xml version="1.0"?>')]
+  F = ['<?xml version="1.0" encoding="UTF-8"?>\n'+d.strip() for d in F if d.strip()]
+  return F
+
+class Profiler:
+
+  def __init__(self, raw:dict|None=None, exclude=None):
+    self.Q = raw if raw is not None else {}
+    self.E = exclude if exclude is not None else []
+    self.Y = []
+
+  def update(self, d:dict|list, path0:str='/'):
+    rep = set()
+    for k, V in d.items():
+      path = path0+k+'/'
+      if any(k in path for k in self.E): continue
+      for v in V if isinstance(V, list) else [V]:
+        if path not in self.Q.keys():
+          self.Q[path] = dict()
+          self.Q[path]['repeat'] = False
+        if k in rep: self.Q[path]['repeat'] = True
+        else: rep.add(k)
+        if isinstance(v, dict): self.update(v, path)
+        elif v is None: self.Q[path]['None'] = True
+    return self
+
+  def apply(self, d:dict|list, path0:str='/', y:dict|None=None):
+    if (y is None) or self.Q[path0]["repeat"]:
+      y0 = y if y is not None else None
+      y = { "id": str(unique()), "path": path0 }
+      if y0 is not None: y[ "&" + y0['path'] ] = y0['id']
+      self.Y.append(y)
+    for k, V in d.items():
+      path = path0+k+'/'
+      if any(k in path for k in self.E): continue
+      for i, v in enumerate(V if isinstance(V, list) else [V]):
+        if isinstance(v, dict): self.apply(v, path, y)
+        else: y[path] = v
+    return self
+
+  def JSONl(self, dir:str, listname:str):
+    F = [os.path.join(dir, f) for f in os.listdir(dir) if f.lower().endswith('.json')]
+    for f0 in progress(F, desc=dir):
+      with open(f0) as f: D = json.load(f)[listname]
+      for d in D: self.update(d, dir)
+    for i, f0 in progress(enumerate(F), desc=dir, total=len(F)):
+      with open(f0) as f: D = json.load(f)[listname]
+      for d in D: self.apply(d, dir)
+    return self
+
+  def JSON(self, dir:str):
+    F = [os.path.join(dir, f) for f in os.listdir(dir) if f.lower().endswith('.json')]
+    for f0 in progress(F, desc=dir):
+      with open(f0) as f: d = json.load(f)
+      self.update(d, dir)
+    for i, f0 in progress(enumerate(F), desc=dir, total=len(F)):
+      with open(f0) as f: d = json.load(f)
+      self.apply(d, dir)
+    return self
+
+  def XML(self, dir:str):
+    F = [os.path.join(dir, f) for f in os.listdir(dir) if f.lower().endswith('.xml')]
+    for f0 in progress(F, desc=dir):
+      with open(f0) as f: d = f.read()
+      self.update(xmltodict.parse(d), dir)
+    for i, f0 in progress(enumerate(F), desc=dir, total=len(F)):
+      with open(f0) as f: d = f.read()
+      self.apply(xmltodict.parse(d), dir)
+    return self
+  
+  def XMLb(self, dir:str):
+    F = [os.path.join(dir, f) for f in os.listdir(dir) if f.lower().endswith('.xml')]
+    for f0 in progress(F, desc=dir):
+      B = xmlbundleload(f0)
+      for b in B: self.update(xmltodict.parse(b), dir)
+    for i, f0 in progress(enumerate(F), desc=dir, total=len(F)):
+      B = xmlbundleload(f0)
+      for b in B: self.apply(xmltodict.parse(b), dir)
+    return self
+
+  def dataframes(self):
+    return { n: X.drop(columns=['path']).dropna(axis=1, how='all')
+                for n, X in DataFrame(self.Y).groupby('path') }
--- node/profile.py
+++ node/profile.py
@@ -1,7 +1,4 @@
-from pandas import DataFrame
-import json, xmltodict, re, networkx as nx
-from tqdm import tqdm as progress
-from uuid import uuid1 as unique
+import re, networkx as nx
 import sys, os, re, pickle
 
 DIR = os.path.dirname(__file__)
@@ -10,94 +7,7 @@ sys.path.append(ROOT) # dodanie lib
 os.chdir(DIR) # zmiana katalogu dla procesów
 
 from lib.log import log, notify
-
-class Profile:
-
-  def __init__(self, raw:dict|None=None, exclude=None):
-    self.Q = raw if raw is not None else {}
-    self.E = exclude if exclude is not None else []
-    self.Y = []
-
-  def update(self, d:dict|list, path0:str='/'):
-    rep = set()
-    for k, V in d.items():
-      path = path0+k+'/'
-      if any(k in path for k in self.E): continue
-      for v in V if isinstance(V, list) else [V]:
-        if path not in self.Q.keys():
-          self.Q[path] = dict()
-          self.Q[path]['repeat'] = False
-        if k in rep: self.Q[path]['repeat'] = True
-        else: rep.add(k)
-        if isinstance(v, dict): self.update(v, path)
-        elif v is None: self.Q[path]['None'] = True
-    return self
-
-  def apply(self, d:dict|list, path0:str='/', y:dict|None=None):
-    if (y is None) or self.Q[path0]["repeat"]:
-      y0 = y if y is not None else None
-      y = { "id": str(unique()), "path": path0 }
-      if y0 is not None: y[ "&" + y0['path'] ] = y0['id']
-      self.Y.append(y)
-    for k, V in d.items():
-      path = path0+k+'/'
-      if any(k in path for k in self.E): continue
-      for i, v in enumerate(V if isinstance(V, list) else [V]):
-        if isinstance(v, dict): self.apply(v, path, y)
-        else: y[path] = v
-    return self
-
-  def JSONl(self, dir:str, listname:str):
-    F = [os.path.join(dir, f) for f in os.listdir(dir) if f.lower().endswith('.json')]
-    for f0 in progress(F, desc=dir):
-      with open(f0) as f: D = json.load(f)[listname]
-      for d in D: self.update(d, dir)
-    for i, f0 in progress(enumerate(F), desc=dir, total=len(F)):
-      with open(f0) as f: D = json.load(f)[listname]
-      for d in D: self.apply(d, dir)
-    return self
-
-  def JSON(self, dir:str):
-    F = [os.path.join(dir, f) for f in os.listdir(dir) if f.lower().endswith('.json')]
-    for f0 in progress(F, desc=dir):
-      with open(f0) as f: d = json.load(f)
-      self.update(d, dir)
-    for i, f0 in progress(enumerate(F), desc=dir, total=len(F)):
-      with open(f0) as f: d = json.load(f)
-      self.apply(d, dir)
-    return self
-
-  def XML(self, dir:str):
-    F = [os.path.join(dir, f) for f in os.listdir(dir) if f.lower().endswith('.xml')]
-    for f0 in progress(F, desc=dir):
-      with open(f0) as f: d = f.read()
-      self.update(xmltodict.parse(d), dir)
-    for i, f0 in progress(enumerate(F), desc=dir, total=len(F)):
-      with open(f0) as f: d = f.read()
-      self.apply(xmltodict.parse(d), dir)
-    return self
-  
-  def XMLb(self, dir:str):
-    F = [os.path.join(dir, f) for f in os.listdir(dir) if f.lower().endswith('.xml')]
-    for f0 in progress(F, desc=dir):
-      B = Profile.xmlbundleload(f0)
-      for b in B: self.update(xmltodict.parse(b), dir)
-    for i, f0 in progress(enumerate(F), desc=dir, total=len(F)):
-      B = Profile.xmlbundleload(f0)
-      for b in B: self.apply(xmltodict.parse(b), dir)
-    return self
-
-  @staticmethod
-  def xmlbundleload(f0):
-    with open(f0) as f: F = f.read()
-    F = F.replace("&", "?").split('<?xml version="1.0" encoding="UTF-8"?>')
-    F = [d for d0 in F for d in d0.split('<?xml version="1.0"?>')]
-    F = ['<?xml version="1.0" encoding="UTF-8"?>\n'+d.strip() for d in F if d.strip()]
-    return F
-
-  def dataframes(self):
-    return { n: X.drop(columns=['path']).dropna(axis=1, how='all')
-                for n, X in DataFrame(self.Y).groupby('path') }
+from lib.profile import Profiler
 
 def branches(r:str, E):
   G = nx.DiGraph()
@@ -136,20 +46,20 @@ class Mermaid:
 
 log("✨"); notify("✨")
 
-pyalex = Profile(exclude=["abstract_inverted_index", "updated_date", "created_date"])\
+pyalex = Profiler(exclude=["abstract_inverted_index", "updated_date", "created_date"])\
         .JSON("api.openalex.org/res/").dataframes()
 with open("api.openalex.org/data.pkl", 'wb') as f: pickle.dump(pyalex, f)
 Mermaid.schema(pyalex, "api.openalex.org/readme.md", "api.openalex.org/res/")
 log("📑", pyalex.keys())
 notify("📑 pyalex ✅")
 
-lens = Profile().JSONl("api.lens.org/res/", listname="data").dataframes()
+lens = Profiler().JSONl("api.lens.org/res/", listname="data").dataframes()
 with open("api.lens.org/data.pkl", 'wb') as f: pickle.dump(lens, f)
 Mermaid.schema(lens, "api.lens.org/readme.md", "api.lens.org/res/")
 log("📑", lens.keys())
 notify("📑 lens ✅")
 
-uprp = Profile().XML("api.uprp.gov.pl/raw/").dataframes()
+uprp = Profiler().XML("api.uprp.gov.pl/raw/").dataframes()
 with open("api.uprp.gov.pl/data.pkl", 'wb') as f: pickle.dump(uprp, f)
 Mermaid.schema(uprp, "api.uprp.gov.pl/readme.md", "api.uprp.gov.pl/raw/")
 log("📑", uprp.keys())
