f5181757bf077b7b75119a9029c11065488c0ec9
Adam Jakubczak <adam.per.jakubczak@gmail.com>
2025-01-20 18:36:20 +0100

    dodanie klasy Colr z definicjami kolorów oraz konfiguracja matplotlib do wyłączenia górnych i prawych spine'ów

--- config.py
+++ config.py
@@ -3,3 +3,15 @@ data = { 'UPRP': 'api.uprp.gov.pl',
          'USPG': 'developer.uspto.gov/grant',
          'USPA': 'developer.uspto.gov/application',
          'Google': 'patents.google.com' }
+
+class Colr:
+
+  neutral = 'blue'
+  good = 'green'
+  mid = 'yellow'
+  attention = 'orange'
+  warning = 'red'
+
+import matplotlib.pyplot as plt
+plt.rcParams['axes.spines.top'] = False
+plt.rcParams['axes.spines.right'] = False
