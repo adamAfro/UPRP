7df743cf47f10efb6d73205068795aef7a252991
Adam Jakubczak <adam.per.jakubczak@gmail.com>
2025-02-02 17:50:45 +0100

    zmiana lokacji funk. grafu rprt

--- endo.py
+++ endo.py
@@ -1,6 +1,6 @@
 import pandas, numpy, geopandas as gpd, altair as Plot
 from lib.flow import Flow
-import geoloc, subject, raport, lib.timeseries
+import geoloc, subject, lib.timeseries
 
 @Flow.From()
 def meandist(geo:pandas.DataFrame, dist:pandas.DataFrame, coords:list[str], rads=[], filtr=None, symbol=''):
@@ -42,31 +42,6 @@ def meandist(geo:pandas.DataFrame, dist:pandas.DataFrame, coords:list[str], rads
 
   return X
 
-@Flow.From()
-def graph(edgdocs:pandas.DataFrame, 
-          edgaffil:pandas.DataFrame,
-          dist:pandas.DataFrame):
-
-  A = edgaffil.reset_index()
-
-  D = edgdocs
-  D['edge'] = range(len(D))
-
-  X = A.set_index('doc').join(D.set_index('from'), how='inner')
-  Y = A.set_index('doc').join(D.set_index('to'), how='inner')
-  E = X.set_index('edge').join(Y.set_index('edge'), rsuffix='Y', how='inner').reset_index()
-
-  E = E.set_index(['lat', 'lon', 'latY', 'lonY'])
-
-  d = dist.reset_index().melt(id_vars=[('lat', ''), ('lon', '')], value_name='distance')
-  d.columns = ['lat', 'lon', 'latY', 'lonY', 'distance']
-  d = d.set_index(['lat', 'lon', 'latY', 'lonY'])
-  E = E.join(d)
-
-  E['closeness'] = 1 / (E['distance']+1)
-
-  return E
-
 data = subject.mapped
 data = meandist(data, geoloc.dist, coords=['lat', 'lon'], rads=[50, 100])
 for k in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']:
@@ -74,10 +49,6 @@ for k in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']:
                   filtr=lambda X, k=k: X[f'clsf-{k}'] > 0, symbol=k)
 
 data = data.map('endo/final.pkl')
-
-rprtgraph = graph(raport.valid, data, geoloc.dist)
-
-debug = { 'rprtgraph': rprtgraph }
 
 plots = dict()
 
@@ -305,63 +276,6 @@ for r in ['', '50', '100']:
   )(X.value_counts(['lat', 'lon', f'meandist{r}']).reset_index()) | Plot.hconcat(*([
     histogram(data, f'meandist{k}{r}', title=k)() for k in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
   ]+[histogram(data, f'meandist{r}')()])))
-
-plots[f'M-rprtdist'] = Flow(args=[rprtgraph, geoloc.region[1]], callback=lambda X, G: (
-
-  lambda X, G=G:
-
-    Plot.vconcat(
-
-      Plot.Chart(G).mark_geoshape(stroke='black', fill=None) + \
-
-      X .query(f'distance == 0')\
-        .pipe(Plot.Chart)\
-        .mark_circle(fill='green')\
-        .encode(Plot.Latitude('lat'), 
-                Plot.Longitude('lon'),
-                Plot.Size('count()')\
-                    .legend(orient='top')\
-                    .title('Ilość połączeń w tej samej geolokalizacji')), *[
-
-      Plot.Chart(G).mark_geoshape(stroke='black', fill=None) + \
-
-      X .query(f'{start} < distance <= {end}')\
-        .pipe(Plot.Chart, title=f'{start} - {end} km')\
-        .mark_rule(opacity=0.1)\
-        .encode(Plot.Color('distance:Q')\
-                    .title('Dystans [km]')\
-                    .scale(scheme='greens', reverse=True)\
-                    .legend(orient='bottom'),
-                Plot.Latitude('lat'),
-                Plot.Longitude('lon'),
-                Plot.Latitude2('latY'),
-                Plot.Longitude2('lonY')).project('mercator')
-
-        for start, end in [(0, 100), (100, 200)]]) | \
-
-    Plot.vconcat(*[
-
-      Plot.Chart(G).mark_geoshape(stroke='black', fill=None) + \
-
-      X .query(f'{start} < distance <= {end}')\
-        .pipe(Plot.Chart, title=f'{start} - {end} km')\
-        .mark_rule(opacity=0.1)\
-        .encode(Plot.Color('distance:Q')\
-                    .title('Dystans [km]')\
-                    .scale(scheme='greens', reverse=True)\
-                    .legend(orient='bottom'),
-                Plot.Latitude('lat'),
-                Plot.Longitude('lon'),
-                Plot.Latitude2('latY'),
-                Plot.Longitude2('lonY')).project('mercator')
-
-        for start, end in [(200, 300), (300, 450), (450, 800)]])
-
-  )(X.reset_index()[['lat', 'lon', 'latY', 'lonY', 'distance', 'closeness']]))
-
-
-plots['F-rprt-meandist'] = histogram(rprtgraph, 'distance', step=10)
-
 
 for k, F in plots.items():
   F.name = k
