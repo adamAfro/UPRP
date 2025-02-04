8bdc468f78a7ee109f56f1f523fb6b122eb8eee8
Adam Jakubczak <adam.per.jakubczak@gmail.com>
2025-02-04 15:46:46 +0100

    dodanie stat. Jaccarda dla krawędzi i wykres

--- grph.py
+++ grph.py
@@ -1,6 +1,6 @@
 import pandas, numpy, geopandas as gpd, altair as Plot, altair as Pt
 from lib.flow import Flow
-import gloc, endo, raport as rprt
+import lib.flow, gloc, endo, raport as rprt
 from util import A4
 
 @Flow.From()
@@ -26,11 +26,18 @@ def graph(edgdocs:pandas.DataFrame,
   d = dist.reset_index().melt(id_vars=[('lat', ''), ('lon', '')], value_name='distance')
   d.columns = ['lat', 'lon', 'latY', 'lonY', 'distance']
   d = d.set_index(['lat', 'lon', 'latY', 'lonY'])
-  E = E.join(d)
+  E = E.join(d).reset_index()
 
   E['closeness'] = 1 / (E['distance']+1)
   E['Gdelay'] = (E['grantY'] - E['grant']).dt.days.astype(int)
   E['Adelay'] = (E['applicationY'] - E['application']).dt.days.astype(int)
+
+  E = E[E['Adelay'] > 0]
+
+ #Jaccard
+  JX = E.apply(lambda x: frozenset([l for l in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'] if x[f'clsf-{l}'] > 0]), axis=1)
+  JY = E.apply(lambda x: frozenset([l for l in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'] if x[f'clsf-{l}Y'] > 0]), axis=1)
+  E['Jaccard'] = JX.to_frame().join(JY.rename(1)).apply(lambda x: len(x[0] & x[1]) / len(x[0] | x[1]), axis=1)
 
   return E
 
@@ -78,6 +85,53 @@ nodes = findcomp(endo.data, edges)
 comps = statcomp(nodes, edges).map('cache/compstat.pkl')
 
 plots = dict()
+
+plots['F-edges'] = lib.flow.forward(edges, lambda X:(
+
+  lambda X:
+
+    ( X .pipe(Pt.Chart).mark_circle()\
+        .properties(width=0.05*A4.W, height=0.05*A4.H)\
+        .encode(Pt.Y('idgeo:N').title(None),
+                Pt.Size('count(idgeo)').legend(None)) |\
+      X .pipe(Pt.Chart).mark_text()\
+        .properties(width=0.05*A4.W, height=0.05*A4.H)\
+        .encode(Pt.Y('idgeo:N').axis(None),
+                Pt.Text('count(idgeo)')) ) &\
+
+    X .query('distance > 0')\
+      .pipe(Pt.Chart).mark_bar()\
+      .properties(width=0.4*A4.W, height=0.1*A4.H)\
+      .encode(Pt.X('distance:Q')\
+                .title('Niezerowy dystans [km]')\
+                .bin(step=10),
+              Pt.Y('count(distance)')\
+                .title(None)) &\
+
+    X .pipe(Pt.Chart).mark_bar()\
+      .properties(width=0.4*A4.W, height=0.1*A4.H)\
+      .encode(Pt.X('Adelay:Q')\
+                .title('Okres między składaniem aplikacji patentowych [dni]')\
+                .bin(step=100),
+              Pt.Y('count(Adelay)')\
+                .title(None)) &\
+
+   ( X .pipe(Pt.Chart).mark_circle()\
+      .properties(width=0.4*A4.W, height=0.025*A4.H)\
+      .encode(Pt.Size('count(Jaccard)').title(None),
+              Pt.X('Jaccard:Q')\
+                .title(['Liczność krawędzi o danym', 
+                        'indeksie Jaccarda dla klasyfikacji'])) +\
+
+    X .pipe(Pt.Chart).mark_text(yOffset=-0.01*A4.H)\
+      .properties(width=0.4*A4.W, height=0.025*A4.H)\
+      .encode(Pt.X('Jaccard:Q'),
+              Pt.Text('count(Jaccard)')) )
+
+) (X [['distance', 'Jaccard', 'Adelay']]\
+      .eval('idgeo = distance > 0')\
+      .replace({'idgeo': {True: 'd > 0', 
+                          False: '0-wy dystans'}})) )
 
 plots[f'F-rprt-comp'] = Flow.Forward([comps], lambda X:
 (
