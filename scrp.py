94687e39bd03eb3400ac973922760cee74a1c029
Adam Jakubczak <adam.per.jakubczak@gmail.com>
2025-02-09 19:59:00 +0100

    popr. impl. main

---
+++ scrp.py
@@ -0,0 +1,29 @@
+import pandas, re, os, asyncio, aiohttp
+
+from lib.flow import Flow
+import raport as rprt
+
+@Flow.From()
+def Fetch(queries:pandas.Series, URL:str, outdir:str):
+
+  "Pobieranie pełnych stron HTML z wynikami wyszukiwania."
+
+  async def scrap(P:list):
+
+    t = 1
+    H = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    async with aiohttp.ClientSession(headers=H) as S:
+      for i, p in P.iterrows():
+        j, d = p['country'].upper(), ''.join(re.findall(r'\d+', p['number']))
+        o = f"{outdir}/{j}{d}.html"
+        if os.path.exists(o): continue
+        x = f"{URL}/{j}{d}"
+        async with S.get(x) as y0:
+          y = await y0.text()
+          await asyncio.sleep(t)
+          with open(o, "w") as f: f.write(y)
+
+  _, P = queries
+  asyncio.run(scrap(P))
+
+Google = Fetch(rprt.drop, 'https://patents.google.com/patent', outdir='patents.google.com/web')
